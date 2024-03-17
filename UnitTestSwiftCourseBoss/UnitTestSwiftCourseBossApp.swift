524f7487767a1d57771658ed483559f31de1e165
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-03-17 01:03:22 +0100

    Initial Commit

---
+++ UnitTestSwiftCourseBoss/UnitTestSwiftCourseBossApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestSwiftCourseBossApp.swift
+//  UnitTestSwiftCourseBoss
+//
+//  Created by Sidney MALEO on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestSwiftCourseBossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
